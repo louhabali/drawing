e00bfa6b13af521d2d240a0cff93245807cbb3e3
alouhab <louhabali@gmail.com>
2025-05-14 15:19:19 +0100

    done

--- src/geometrical_shapes.rs
+++ src/geometrical_shapes.rs
@@ -1,11 +1,15 @@
-
 use raster::{Image,Color};
 use rand::Rng;
 
 pub trait Drawable {
-     fn draw(&self,img: &mut Image);
-     fn color() -> Color {
-        return Color::rgb(51, 189, 231);
+    fn draw(&self,img: &mut Image);
+    fn color() -> Color {
+        let mut rng = rand::thread_rng();
+        return Color::rgb(
+            rng.gen_range(0..255), 
+            rng.gen_range(0..255),
+            rng.gen_range(0..255)
+        );
     }
 }
 
@@ -13,6 +17,8 @@ pub trait Displayable {
      fn display(&mut self, x: i32, y: i32, color: Color);
 }
 
+// point implementation
+#[derive(Clone)]
 pub struct Point {
     pub x : i32,
     pub y : i32,
@@ -33,112 +39,175 @@ pub struct Point {
             }
     }
 }
+impl  Drawable for Point {
+    fn draw(&self, img : &mut Image){
+       
+        img.display(self.x, self.y,Point::color());
+    }
+}
 
+// line implementation
 pub struct Line {
     pub p1: Point,
     pub p2: Point,
+    color : Color,
 }
 
 impl Line {
-    pub fn new(p1: Point, p2: Point) -> Self {
-        Line { p1, p2 }
+    pub fn new(p1: Point, p2: Point,color : Color) -> Self {
+        Line { 
+            p1, 
+            p2,
+            color
+         }
     }
 
     pub fn random(width: i32, height: i32) -> Self {
         let mut rng = rand::thread_rng();
-        Line {
-            p1: Point::new(rng.gen_range(0..width), rng.gen_range(0..height)),
-            p2: Point::new(rng.gen_range(0..width), rng.gen_range(0..height)),
-        }
-    }
-}
+        Self::new(
+            Point::new(rng.gen_range(0..width), rng.gen_range(0..height)),
+            Point::new(rng.gen_range(0..width), rng.gen_range(0..height)),
+            Point::color(),
+    )
 
-// pub struct Triangle<'a> {
-//     pub p1 :&'a Point,
-//     pub p2 :&'a Point,
-//     pub p3 :&'a Point,
-// }
-//  impl <'a>Triangle<'a>{
-//     pub fn new(p1 : &'a Point,p2 : &'a Point,p3:&'a Point) -> Self {
-//         Triangle {
-//             p1,
-//             p2,
-//             p3,
-//         }
-
-//     }
-// }
-// pub struct Rectangle  {
-//     pub p1 :  Point,
-//     pub p2 :  Point,
-// }
-//  impl  Rectangle {
-//     pub fn new(p1 : Point,p2 : Point) -> Self {
-//         Rectangle {
-//             p1,
-//             p2,
-//         }
-        
-//     }
-// }
-// pub struct Circle<'a>{
-//     pub center : &'a Point,
-//     pub radius : i32,
-// }
-//  impl <'a>Circle<'a> {
-//     pub fn new(center : &'a Point , radius : i32)-> Self {
-//         Circle {
-//             center,
-//             radius,
-//         }
-//     }
-//     pub fn random(width:i32, height:i32)-> Point {
-//             let mut rng = rand::thread_rng();
-//             Point {
-//                 x : rng.gen_range(0..width),
-//                 y : rng.gen_range(0..height),
-//             }
-//     }
-// }
-// impl Drawable for Line {
-
-// }
-impl  Drawable for Point {
-    fn draw(&self, img : &mut Image){
-        img.set_pixel(self.x, self.y, Point::color());
     }
 }
 impl Drawable for Line {
     fn draw(&self, img : &mut Image){
-    //    img.set_pixel(self.p1.x, self.p1.y, Point::color());
-    //    img.set_pixel(self.p2.x, self.p2.y, Point::color());
-    let dx = (self.p1.x - self.p2.x) as f64;
-    let dy = (self.p1.y - self.p2.y) as f64;
-    let mut newx :f64 =0.0; 
-    let mut newy :f64 =0.0;
+    let dx = (self.p2.x - self.p1.x) as f64;
+    let dy = (self.p2.y - self.p1.y) as f64;
+   
     let steps = dx.abs().max(dy.abs());
     for i in 0..steps as i32  {
-        //println!("{}",i);
         let t = i as f64 / steps; 
-        newx = self.p1.x as f64 + dx * t;
-        newy = self.p1.y as f64 + dy * t;
-        println!("{}---{}",newx,newy);
+        let mut newx = self.p1.x as f64 + dx * t; 
+        let mut newy = self.p1.y as f64 + dy * t;
         newx = newx.round();
         newy = newy.round();
         if newx >= 0.0 && newx < img.width.into() && newy >= 0.0 && newy < img.height.into() {
-             img.set_pixel(newx as i32, newy as i32, Point::color()).unwrap();
+             img.display(newx as i32, newy as i32, self.color.clone());
         }
     }
-    
     }
 }
-// impl  Drawable for Rectangle{
-//     fn draw(&self, img : &mut Image){
-//        //img.
-//     }
-// }
-// impl <'a> Drawable for Triangle <'a>{
-//     fn draw(&self, img : &mut Image){
-//        //img.
-//     }
-// }
+
+// triangle implementation
+pub struct Triangle {
+    pub p1 :Point,
+    pub p2 :Point,
+    pub p3 :Point,
+}
+ impl Triangle{
+    pub fn new(p1 :&Point,p2 : &Point,p3: &Point) -> Self {
+        Triangle {
+            p1:p1.clone(),
+            p2:p2.clone(),
+            p3:p3.clone(),
+        }
+
+    }
+}
+impl  Drawable for Triangle{
+    fn draw(&self, img : &mut Image){
+        let c = Point::color();
+       let l1 = Line {p1 : self.p1.clone(),p2: self.p2.clone(),color:c.clone()};
+       let l2 = Line {p1 : self.p2.clone(),p2: self.p3.clone(),color:c.clone()};
+       let l3 = Line {p1 : self.p3.clone(),p2: self.p1.clone(),color:c.clone()};
+       l1.draw(img);
+       l2.draw(img);
+       l3.draw(img);
+    }
+}
+
+
+// rectangle implementation
+pub struct Rectangle  {
+    pub p1 :  Point,
+    pub p2 :  Point,
+}
+ impl  Rectangle {
+    pub fn new(p1 : &Point,p2 : &Point) -> Self {
+        Rectangle {
+            p1 : p1.clone(),
+            p2 : p2.clone(),
+        }
+        
+    }
+}
+impl  Drawable for Rectangle{
+    fn draw(&self, img : &mut Image){
+        let p3 = Point::new(self.p1.x , self.p2.y);
+        let p4 = Point::new(self.p2.x , self.p1.y);
+        let c = Point::color();
+       let l1 = Line {p1 : self.p2.clone(),p2 : p3.clone(),color:c.clone()};
+       let l2 = Line {p1 : p3.clone(), p2 : self.p1.clone(),color:c.clone()};
+       let l3 = Line {p1:self.p1.clone(),p2: p4.clone(),color:c.clone()};
+       let l4 = Line {p1: p4.clone() ,p2 : self.p2.clone(),color:c.clone()};
+       l1.draw(img);
+       l2.draw(img);
+       l3.draw(img);
+       l4.draw(img);
+    }
+}
+
+// circle implementation
+pub struct Circle{
+    pub center : Point,
+    pub radius : i32,
+}
+ impl Circle {
+    pub fn new(center : &Point , radius : i32)-> Self {
+        Circle {
+           center: center.clone(),
+        radius,
+        }
+    }
+    pub fn random(width:i32, height:i32)-> Circle {
+            let mut rng = rand::thread_rng();
+            Self::new( 
+                &Point::random(width,height),
+                rng.gen_range(0..height)
+            )
+    }
+}
+fn calculate_distance(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    (dx.powi(2) + dy.powi(2)).sqrt()
+}
+impl Drawable for Circle{
+    fn draw(&self, img : &mut Image){
+        let cx= self.center.x;
+        let cy= self.center.y;
+        let r= self.radius as f64;
+        let mycolor = Self::color();
+        let mut x=cx;
+        let mut y=cy-r as i32;
+
+       while y<=cy {
+        let a=(calculate_distance(cx as f64,cy as f64,(x+1) as f64,y as f64) -r).abs();
+        let b=(calculate_distance(cx as f64,cy as f64,x as f64,(y+1) as f64) -r).abs();
+        let c=(calculate_distance(cx as f64,cy as f64,(x+1) as f64,(y+1) as f64) -r).abs();
+        let min= a.min(b).min(c);
+
+        if a==min{
+            x+=1;
+        }else if b==min{
+            y+=1;
+        }else if c==min{
+            x+=1;
+            y+=1;
+        }
+        img.display(x, y, mycolor.clone());
+        img.display(x, cy + (cy - y), mycolor.clone());
+        img.display(2 * cx - x, y,  mycolor.clone());
+        img.display(2 * cx - x, cy + (cy - y), mycolor.clone());
+       }
+
+
+    }
+}
+
+
+
+
